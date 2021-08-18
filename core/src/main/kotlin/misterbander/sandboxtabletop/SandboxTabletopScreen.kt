39ee7552977515860030c485135a159f830dee33
Mister_Bander <kttan1999@gmail.com>
2021-08-18 18:10:21 +0800

    refactor: Use KtxInputListener instead of InputListener

--- core/src/main/kotlin/misterbander/sandboxtabletop/SandboxTabletopScreen.kt
+++ core/src/main/kotlin/misterbander/sandboxtabletop/SandboxTabletopScreen.kt
@@ -3,8 +3,8 @@ package misterbander.sandboxtabletop
 import com.badlogic.gdx.audio.Sound
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.scenes.scene2d.InputEvent
-import com.badlogic.gdx.scenes.scene2d.InputListener
 import com.badlogic.gdx.utils.viewport.ExtendViewport
+import ktx.actors.KtxInputListener
 import misterbander.gframework.GScreen
 
 abstract class SandboxTabletopScreen(game: SandboxTabletop) : GScreen<SandboxTabletop>(game)
@@ -15,7 +15,7 @@ abstract class SandboxTabletopScreen(game: SandboxTabletop) : GScreen<SandboxTab
 	
 	val click: Sound = game.assetManager["sounds/click.wav"]
 	
-	val ignoreTouchDown = object : InputListener()
+	val ignoreTouchDown = object : KtxInputListener()
 	{
 		override fun touchDown(event: InputEvent, x: Float, y: Float, pointer: Int, button: Int): Boolean
 		{

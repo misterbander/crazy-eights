92e9171ba71610cb4a70f6c1ff8b4610c36df332
Mister_Bander <kttan1999@gmail.com>
2021-11-27 01:59:08 +0800

    fix: Jitter while rotating and moving cards into own hand

--- core/src/main/kotlin/misterbander/crazyeights/scene2d/transform.kt
+++ core/src/main/kotlin/misterbander/crazyeights/scene2d/transform.kt
@@ -13,14 +13,22 @@ import kotlin.math.floor
 
 fun GObject<*>.transformToGroupCoordinates(group: Group)
 {
-	val smoothMovable = getModule<SmoothMovable>()!!
-	val (newX1, newY1) = localToActorCoordinates(group, tempVec.set(0F, 0F))
-	val (newX2, newY2) = localToActorCoordinates(group, tempVec.set(1F, 0F))
-	val rotation = smoothMovable.rotationInterpolator.target
-	val newRotation = floor(MathUtils.atan2(newY2 - newY1, newX2 - newX1)*MathUtils.radDeg)
-	smoothMovable.setPositionAndTargetPosition(newX1, newY1)
-	smoothMovable.rotationInterpolator.set(newRotation)
-	getModule<Draggable>()!!.unrotatedDragPositionVec.rotateDeg(newRotation - rotation)
+	getModule<SmoothMovable>()!!.apply {
+		val dx = xInterpolator.target - xInterpolator.value
+		val dy = yInterpolator.target - yInterpolator.value
+		val dRotation = rotationInterpolator.target - rotationInterpolator.value
+		val (newX1, newY1) = localToActorCoordinates(group, tempVec.set(0F, 0F))
+		val (newX2, newY2) = localToActorCoordinates(group, tempVec.set(1F, 0F))
+		val rotation = rotationInterpolator.target
+		val newRotation = floor(MathUtils.atan2(newY2 - newY1, newX2 - newX1)*MathUtils.radDeg)
+		xInterpolator.target = newX1 + dx
+		xInterpolator.value = newX1
+		yInterpolator.target = newY1 + dy
+		yInterpolator.value = newY1
+		rotationInterpolator.target = newRotation + dRotation
+		rotationInterpolator.value = newRotation
+		getModule<Draggable>()!!.unrotatedDragPositionVec.rotateDeg(newRotation - rotation + dRotation)
+	}
 }
 
 fun getSpreadPositionForIndex(index: Int, spreadCardCount: Int, separation: Float, curvature: Float): Vector2

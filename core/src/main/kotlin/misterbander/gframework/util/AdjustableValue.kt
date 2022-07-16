5316b6e83d785e6cff83b002e5eb868496182384
misterbander <61056749+misterbander@users.noreply.github.com>
2022-07-16 11:01:43 +0800

    feat: Add options to specify positive and negative keys for AdjustableValue

--- core/src/main/kotlin/misterbander/gframework/util/AdjustableValue.kt
+++ core/src/main/kotlin/misterbander/gframework/util/AdjustableValue.kt
@@ -1,26 +1,24 @@
 package misterbander.gframework.util
 
 import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.Input
 import kotlin.reflect.KProperty
 
 class AdjustableValue(
 	private var value: Float,
-	private val filterKey: Int = -1,
+	private val positiveKey: Int,
+	private val negativeKey: Int,
 	private val adjustingSpeed: Float
 )
 {
 	operator fun getValue(from: Any?, property: KProperty<*>): Float
 	{
-		if (filterKey != -1 && !Gdx.input.isKeyPressed(filterKey))
-			return value
 		var changed = false
-		if (Gdx.input.isKeyPressed(Input.Keys.EQUALS))
+		if (Gdx.input.isKeyPressed(positiveKey))
 		{
 			value += Gdx.graphics.deltaTime*adjustingSpeed
 			changed = true
 		}
-		if (Gdx.input.isKeyPressed(Input.Keys.MINUS))
+		if (Gdx.input.isKeyPressed(negativeKey))
 		{
 			value -= Gdx.graphics.deltaTime*adjustingSpeed
 			changed = true
